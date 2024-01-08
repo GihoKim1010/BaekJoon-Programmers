f4c9269589751263bf4452cc743ea003edeeba4b
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-01-08 14:21:43 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	static int recur(int N, int count) {
+		if (N < 2) {
+			return count;
+		}
+		return Math.min(recur(N / 2, count + 1 + (N % 2)), recur(N / 3, count + 1 + (N % 3)));
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.valueOf(br.readLine());
+		int result = 0;
+
+		bw.write(String.valueOf(recur(N, result)));
+		bw.newLine();
+		bw.flush();
+
+		br.close();
+		bw.close();
+	}
+}
