24f934e7622893332a9bddc950dcea8f0624d4d5
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-01-12 15:00:12 +0900

    [level Lv.0] Title: 첫 번째로 나오는 음수, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,14 @@
+	class Solution {
+		public int solution(int[] num_list) {
+	    	
+	    	int index =-1;
+	    	for(int i=0; i<num_list.length; i++) {
+	    		if(num_list[i]<0) {
+	    			index=i;
+	    			break;
+	    		}
+	    	}
+	    	int answer=index;
+	        return answer;
+	    }
+	}
