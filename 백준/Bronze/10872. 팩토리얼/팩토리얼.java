12d558ba56068bee002105b6e8472cb0fec35b90
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-01-12 09:39:32 +0900

    [Bronze V] Title: 팩토리얼, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		int result = 1;
+
+		if (N == 0) {
+			result = 1;
+		} else {
+			for (int i = 1; i <= N; i++) {
+				result *= i;
+			}
+		}
+		bw.write(String.valueOf(result));
+		bw.newLine();
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
