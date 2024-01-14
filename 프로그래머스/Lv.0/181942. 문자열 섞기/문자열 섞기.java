40cb1020ebb6ba5fe7212d2756f07bb64f92c795
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-01-14 17:38:15 +0900

    [level Lv.0] Title: 문자열 섞기, Time: 0.03 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public String solution(String str1, String str2) {
+    	StringBuilder result = new StringBuilder();
+    	for(int i =0; i<str1.length(); i++) {
+    		result.append(str1.charAt(i)).append(str2.charAt(i));
+    	}
+        String answer = result.toString();
+        return answer;
+    }
+}
