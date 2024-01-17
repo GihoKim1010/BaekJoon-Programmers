1379631a684ea943a6956cbed1d0501266834f99
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-01-17 17:28:59 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int y=x;
+        int sum=0;
+        while(y>0) {
+        	sum+=y%10;
+        	y/=10;
+        }
+        
+        if(x%sum!=0) {
+        	answer=false;
+        }
+        return answer;
+    }
+}
