b9c565aafad8befd46e231b9fef97485f1739669
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-01-17 15:06:59 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.12 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class Solution {
+	public int solution(int[] ingredient) {
+
+		Stack<Integer> st = new Stack<>();
+
+		int answer = 0;
+		for (int i = 0; i < ingredient.length; i++) {
+			st.add(ingredient[i]);
+
+			if (st.size() >= 4) {
+				if (st.get(st.size() - 1) == 1 && st.get(st.size() - 2) == 3 && st.get(st.size() - 3) == 2
+						&& st.get(st.size() - 4) == 1) {
+					answer++;
+					st.pop();
+					st.pop();
+					st.pop();
+					st.pop();
+				}
+			}
+		}
+
+		return answer;
+	}
+}
