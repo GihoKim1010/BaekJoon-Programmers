c5e76758c331dd0c0d401595ee972e2a36921353
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-01-18 15:45:42 +0900

    [level 1] Title: 예산, Time: 0.48 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        Arrays.sort(d);
+        
+        for(int i=0; i<d.length; i++) {
+        	budget -=d[i];
+        	if(budget<0) {
+        		break;
+        	}else if(budget==0) {
+        		answer++;
+        		break;
+        	}else {
+        		answer++;
+        	}
+        }
+        return answer;
+    }
+}
