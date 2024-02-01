5267d843c02d41b6c99bafc4fa7a5322a04ac898
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-02-01 16:29:21 +0900

    [Gold IV] Title: 오큰수, Time: 1148 ms, Memory: 200492 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		Stack<int[]> nums = new Stack<>();
+
+		int N = Integer.valueOf(br.readLine());
+		String input = br.readLine();
+		String[] sp = input.split(" ");
+		int[] answer = new int[N];
+		Arrays.fill(answer, -1);
+
+		for (int i = 0; i < N; i++) {
+			int z = Integer.parseInt(sp[i]);
+			int[] temp = { i, z };
+			while (true) {
+				if (!nums.isEmpty() && nums.peek()[1] < z) {
+					answer[nums.peek()[0]] = z;
+					nums.pop();
+				}else {
+					nums.add(temp);
+					break;
+				}
+			}
+		}
+		StringBuilder result = new StringBuilder();
+		
+		for(int x:answer) {
+			result.append(x+" ");
+		}
+
+		bw.write(result.toString());
+		bw.newLine();
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
