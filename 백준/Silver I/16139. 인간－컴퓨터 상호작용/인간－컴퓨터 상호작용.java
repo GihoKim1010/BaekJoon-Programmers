90e9f3615ec4537ef2eed0f499a29b6f23977213
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-02-16 10:31:05 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 972 ms, Memory: 122596 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver I/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver I/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,11 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		int q=sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String s = br.readLine();
+		int q=Integer.parseInt(br.readLine());
 		int[][] ar = new int[s.length()+1][26];
 		
 		for(int i=0; i<s.length(); i++) {
@@ -16,12 +22,18 @@ public class Main {
 		}
 		StringBuilder result = new StringBuilder();
 		for(int i=0; i<q; i++) {
-			String a = sc.next();
+			String input = br.readLine();
+			String[] temp = input.split(" ");
+			String a = temp[0];
 			int x = a.charAt(0)-'a';
-			int l = sc.nextInt(), r=sc.nextInt();
+			int l = Integer.parseInt(temp[1]), r=Integer.parseInt(temp[2]);
 			result.append(ar[r+1][x]-ar[l][x]+"\n");
 		}
 		
-		System.out.println(result.toString());
+		bw.write(result.toString());
+		bw.newLine();
+		bw.flush();
+		br.close();
+		bw.close();
 	}
 }
