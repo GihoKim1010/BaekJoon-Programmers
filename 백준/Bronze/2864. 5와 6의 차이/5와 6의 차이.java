4023cd2bf8cdc17b72f49968f3ff71b004f4cfbe
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-02-27 17:45:32 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 232 ms, Memory: 18352 KB -BaekjoonHub

---
+++ 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next(), b = sc.next();
+
+		a = a.replaceAll("5", "6");
+		b = b.replaceAll("5", "6");
+		int max = Integer.parseInt(a) + Integer.parseInt(b);
+
+		a = a.replaceAll("6", "5");
+		b = b.replaceAll("6", "5");
+		int min = Integer.parseInt(a) + Integer.parseInt(b);
+
+		System.out.println(min + " " + max);
+	}
+}
