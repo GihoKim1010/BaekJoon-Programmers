0fb1705d73870569623e3b3e54ffab570bb97673
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-03-06 14:56:51 +0900

    [Silver II] Title: 이동하기, Time: 1476 ms, Memory: 328148 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt(), m=sc.nextInt();
+		int[][] maze = new int[n+1][m+1];
+		int[][] dp = new int[n+1][m+1];
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=m; j++) {
+				maze[i][j]=sc.nextInt();
+			}
+		}
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=m; j++) {
+				dp[i][j]=maze[i][j]+Math.max(dp[i-1][j], dp[i][j-1]);
+			}
+		}
+		System.out.println(dp[n][m]);
+	}
+}
