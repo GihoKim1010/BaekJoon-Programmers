58f29163b928eadb78fa3e44b5e3438bdeb35879
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-04-26 21:45:04 +0900

    [Bronze IV] Title: 과목선택, Time: 204 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/11948. 과목선택/과목선택.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int[] a = new int[4];
+		int[] b = new int[2];
+		
+		for(int i=0; i<4; i++) {
+			a[i]=sc.nextInt();
+		}
+		for(int i=0; i<2; i++) {
+			b[i]=sc.nextInt();
+		}
+		Arrays.sort(a);
+		Arrays.sort(b);
+		
+		int sum = 0;
+		for(int i=1; i<4; i++) {
+			sum+=a[i];
+		}
+		
+		System.out.println(sum+b[1]);
+	}
+}
