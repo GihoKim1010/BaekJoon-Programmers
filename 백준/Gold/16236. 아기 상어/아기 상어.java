728714d2a2840d74b88f758481a484297e531f3d
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-05-17 16:37:16 +0900

    [Gold III] Title: 아기 상어, Time: 248 ms, Memory: 18596 KB -BaekjoonHub

---
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -0,0 +1,79 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+	static int[] dy = { -1, 0, 0, 1 };
+	static int[] dx = { 0, -1, 1, 0 };
+	static int[][] map;
+	static int[] cur;
+	static int N, eat, size, move;
+	static boolean hasEaten;
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+
+		map = new int[N][N];
+
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++) {
+				map[i][j] = sc.nextInt();
+				if (map[i][j] == 9) {
+					cur = new int[] { i, j };
+					map[i][j] = 0;
+				}
+			}
+
+		size = 2;
+		eat = 0;
+		move = 0;
+
+		hasEaten=true;
+		while (hasEaten) {
+			bfs();
+			if (size == eat) { // 사이즈와 먹이를 먹은 수가 동일하다면 상어의 크기를 증가
+				size++;
+				eat = 0;
+			}
+		}
+
+		System.out.println(move);
+
+	}
+
+	static void bfs() {
+		PriorityQueue<int[]> que = new PriorityQueue<>((o1, o2) -> o1[2] != o2[2] ? Integer.compare(o1[2], o2[2])
+				: (o1[0] != o2[0] ? Integer.compare(o1[0], o2[0]) : Integer.compare(o1[1], o2[1])));
+		boolean[][] visit = new boolean[N][N];
+
+		//y, x, 이동거리
+		que.add(new int[] { cur[0], cur[1], 0 });
+		visit[cur[0]][cur[1]] = true;
+
+		hasEaten=false;
+		
+		while (!que.isEmpty()) {
+			cur = que.poll();
+
+			if (map[cur[0]][cur[1]] != 0 && map[cur[0]][cur[1]] < size) { 
+				map[cur[0]][cur[1]] = 0;
+				eat++;
+				move += cur[2];
+				hasEaten = true;
+				break;
+			}
+
+			for (int k = 0; k < 4; k++) {
+				int ny = cur[0] + dy[k];
+				int nx = cur[1] + dx[k];
+
+				if (ny < 0 || nx < 0 || nx >= N || ny >= N || visit[ny][nx] || map[ny][nx] > size)
+					continue;
+
+				que.add(new int[] { ny, nx, cur[2] + 1 });
+				visit[ny][nx] = true;
+			}
+		}
+	}
+}
