5a59dac8deead619e2c93862b334d85509547107
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-05-25 23:57:23 +0900

    [Bronze V] Title: 웰컴, Time: 192 ms, Memory: 17240 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(".  .   .").append("\n");
+		sb.append("|  | _ | _. _ ._ _  _").append("\n");
+		sb.append("|/\\|(/.|(_.(_)[ | )(/.");
+		
+		System.out.println(sb.toString());
+	}
+}
