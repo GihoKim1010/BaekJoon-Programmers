df11914caf382596e8e6ba132a16f7041c968d85
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-06-05 17:12:11 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 100 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        StringBuilder result = new StringBuilder();
+        result.append("    8888888888  888    88888").append("\n");
+        result.append("   88     88   88 88   88  88").append("\n");
+        result.append("    8888  88  88   88  88888").append("\n");
+        result.append("       88 88 888888888 88   88").append("\n");
+        result.append("88888888  88 88     88 88    888888").append("\n").append("\n");
+        result.append("88  88  88   888    88888    888888").append("\n");
+        result.append("88  88  88  88 88   88  88  88").append("\n");
+        result.append("88 8888 88 88   88  88888    8888").append("\n");
+        result.append(" 888  888 888888888 88  88      88").append("\n");
+        result.append("  88  88  88     88 88   88888888");
+        System.out.println(result.toString());
+
+    }
+}
