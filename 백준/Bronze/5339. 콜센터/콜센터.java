d2c6c9764f25bd73406fabe18e9e08079011a893
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-06-08 11:45:16 +0900

    [Bronze V] Title: 콜센터, Time: 204 ms, Memory: 17040 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		StringBuilder result = new StringBuilder();
+		result.append("     /~\\").append("\n");
+		result.append("    ( oo|").append("\n");
+		result.append("    _\\=/_").append("\n");
+		result.append("   /  _  \\").append("\n");
+		result.append("  //|/.\\|\\\\").append("\n");
+		result.append(" ||  \\ /  ||").append("\n");
+		result.append("============").append("\n");
+		result.append("|          |").append("\n");
+		result.append("|          |").append("\n");
+		result.append("|          |");
+		System.out.println(result.toString());
+	}
+}
