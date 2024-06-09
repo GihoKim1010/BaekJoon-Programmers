5e86512f8cfcbee5d37f5bce0ad8540f4eba9d20
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2024-06-09 22:19:41 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 220 ms, Memory: 17376 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N=sc.nextInt(), K=sc.nextInt();
+		sc.nextLine();
+		for(int i=0; i<N; i++) {
+			String input = sc.nextLine();
+		}
+		System.out.println("비와이");
+	}
+}
